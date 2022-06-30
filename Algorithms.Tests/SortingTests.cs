15e7a534a3944501246441f396e75a34a07f84aa
Nomerovskyi Serhii <nomerushka@gmail.com>
2022-06-30 22:03:03 +0300

    Initial commit

---
+++ Algorithms.Tests/SortingTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Tests
+{
+    public class AlgorithmsTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
---
+++ Algorithms/Sorting.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Algorithms
+{
+    public static class Sorting
+    {
+        public static void BubbleSort(int [] input)
+        {
+            for (int wallIndex = input.Length - 1; wallIndex > 0; wallIndex--)
+            {
+                for(int i = 0; i < input.Length; i++)
+                {
+                    if(input[i] > input[i + 1])
+                    {
+                        Swap(input, i, i + 1);
+                    }
+                }
+            }
+        }
+
+        private static void Swap(int[] array, int i, int j)
+        {
+            if (i == j)
+            {
+                return;
+            }
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }  
+}
