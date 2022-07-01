12385fdf8375c012ac2c74ace69e15bc32318785
Nomerovskyi Serhii <nomerushka@gmail.com>
2022-07-01 09:07:46 +0300

    Bubble sorting and test done

--- Algorithms.Tests/SortingTests.cs
+++ Algorithms.Tests/SortingTests.cs
@@ -1,18 +1,54 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace Algorithms.Tests
 {
-    public class AlgorithmsTests
+    [TestFixture]
+    public class SortingTests
     {
-        [SetUp]
-        public void Setup()
+       private int [][] Samples()
         {
+            int[][] samples = new int[9][];
+            samples[0] = new int[] { 1,10,2,9,3,8,4,7 };
+            samples[1] = new int[] {1};
+            samples[2] = new int[] { 2,8 };
+            samples[3] = new int[] { 1,9, 0 };
+            samples[4] = new int[] { 0,12,-4,14 };
+            samples[5] = new int[] { 9,5,3,2,6 }; 
+            samples[6] = new int[] { 2,3,4,7,1,0,-12,2,67 }; 
+            samples[7] = new int[] { 1,4,3,5,2,7,5,9,7 };
+            samples[8] = new int[] { 9,2,1,-12,-5,6,4,7,8,9 };
+
+            return samples;
         }
+
+        private void RunForSorting(Action<int[]> action)
+        {
+            foreach(var sample in Samples())
+            {
+                action(sample);
+                CollectionAssert.IsOrdered(sample);
+                PrintOut(sample);
+            }
+        }
+
+        private void PrintOut(int[] array)
+        {
+            TestContext.WriteLine("------TRACE------\n");
+
+            foreach(var el in array)
+            {
+                TestContext.Write($"{el} ");
+            }
+
+            TestContext.WriteLine("\n------------\n");
+        }
+
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            RunForSorting(Sorting.BubbleSort);
         }
     }
 }
--- Algorithms/Sorting.cs
+++ Algorithms/Sorting.cs
@@ -8,7 +8,7 @@ namespace Algorithms
         {
             for (int wallIndex = input.Length - 1; wallIndex > 0; wallIndex--)
             {
-                for(int i = 0; i < input.Length; i++)
+                for(int i = 0; i < wallIndex; i++)
                 {
                     if(input[i] > input[i + 1])
                     {
